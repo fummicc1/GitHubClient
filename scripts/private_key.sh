5c431acb978f6415d191fde99fe54faddc9fa82a
fummicc1 <fumiya.tennis1@gmail.com>
2021-08-21 00:16:50 +0900

    Fix

--- scripts/private_key.sh
+++ scripts/private_key.sh
@@ -8,4 +8,4 @@ set -o pipefail
 
 
 # GitHubのプライベートキーを置換する
-cat GitHubClient/Secret/PrivateKey_template.swift | sed -e "s/[GITHUB_API_KEY]/$ACCESS_TOKEN/g" > GitHubClient/Secret/PrivateKey.swift
+cat GitHubClient/Secrets/PrivateKey_template.swift | sed -e "s/\[GITHUB_API_KEY\]/$ACCESS_TOKEN/g" > GitHubClient/Secrets/PrivateKey.swift
