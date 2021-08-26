325bad87dfc58f39b5d283b47871f521ea8e39af
fummicc1 <fumiya.tennis1@gmail.com>
2021-08-26 15:12:29 +0900

    Add Keys

--- GitHubClient/Secrets/PrivateKey_template.swift
+++ GitHubClient/Secrets/PrivateKey_template.swift
@@ -3,4 +3,12 @@ enum PrivateKey {
     static var gitHubAuthToken: String {
         "[GITHUB_API_KEY]"
     }
+
+    static var githubClientID: String {
+        "[GITHUB_CLIENT_ID]"
+    }
+    
+    static var githubClientSecret: String {
+        "[GITHUB_CLIENT_SECRET]"
+    }
 }
--- scripts/private_key.sh
+++ scripts/private_key.sh
@@ -6,6 +6,11 @@ set -e
 # パイプラインの左が失敗したらスクリプトを停止する
 set -o pipefail
 
-
-# GitHubのプライベートキーを置換する
-cat GitHubClient/Secrets/PrivateKey_template.swift | sed -e "s/\[GITHUB_API_KEY\]/$ACCESS_TOKEN/g" > GitHubClient/Secrets/PrivateKey.swift
+# GitHub
+cp GitHubClient/Secrets/PrivateKey_template.swift GitHubClient/Secrets/PrivateKey.swift
+# ACCESS_TOKENを置換する（iで上書きファイル指定, eで処理内容の指定, -i ''でバックアップファイルの拡張子を指定しない）
+sed -i '' -e "s/\[GITHUB_API_KEY\]/$ACCESS_TOKEN/" GitHubClient/Secrets/PrivateKey.swift
+# ClientIDを置換する
+sed -i '' -e "s/\[GITHUB_CLIENT_ID\]/$CLIENT_ID/" GitHubClient/Secrets/PrivateKey.swift
+# ClientSecretを置換する
+sed -i '' -e "s/\[GITHUB_CLIENT_SECRET\]/$CLIENT_SECRET/" GitHubClient/Secrets/PrivateKey.swift
